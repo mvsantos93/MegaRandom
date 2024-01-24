1f88966892d03e66a9cb10fc810a07035a0f552f
Marcos V. Shiuuri <marcosvinicius2610@protonmail.com>
2024-01-24 18:49:49 -0300

    Update

--- database.py
+++ database.py
@@ -1,14 +1,22 @@
 import random
 
-classes = ['datafiles/images/class_null.png','datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png',
-           'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 
-           'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 
-           'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 
-           'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 
-           'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 
-           'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 
-           'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 
-           'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png', 'datafiles/images/class00.png']
+classes = ['datafiles/images/class_null.png', 'datafiles/images/class01.png', 'datafiles/images/class02.png', 'datafiles/images/class03.png', 'datafiles/images/class04.png', 'datafiles/images/class05.png',
+           'datafiles/images/class06.png', 'datafiles/images/class07.png', 'datafiles/images/class08.png', 'datafiles/images/class09.png', 'datafiles/images/class10.png', 'datafiles/images/class11.png', 
+           'datafiles/images/class12.png', 'datafiles/images/class13.png', 'datafiles/images/class14.png', 'datafiles/images/class15.png', 'datafiles/images/class16.png', 'datafiles/images/class17.png', 
+           'datafiles/images/class18.png', 'datafiles/images/class19.png', 'datafiles/images/class20.png', 'datafiles/images/class21.png', 'datafiles/images/class22.png', 'datafiles/images/class23.png', 
+           'datafiles/images/class24.png', 'datafiles/images/class25.png', 'datafiles/images/class26.png', 'datafiles/images/class27.png', 'datafiles/images/class28.png', 'datafiles/images/class29.png', 
+           'datafiles/images/class30.png', 'datafiles/images/class31.png', 'datafiles/images/class32.png', 'datafiles/images/class33.png', 'datafiles/images/class34.png', 'datafiles/images/class35.png', 
+           'datafiles/images/class36.png', 'datafiles/images/class37.png', 'datafiles/images/class38.png', 'datafiles/images/class39.png', 'datafiles/images/class40.png', 'datafiles/images/class41.png', 
+           'datafiles/images/class42.png', 'datafiles/images/class43.png', 'datafiles/images/class44.png', 'datafiles/images/class45.png', 'datafiles/images/class46.png', 'datafiles/images/class47.png', 
+           'datafiles/images/class48.png', 'datafiles/images/class49.png', 'datafiles/images/class50.png', 'datafiles/images/class51.png', 'datafiles/images/class52.png', 'datafiles/images/class53.png', 
+           'datafiles/images/class54.png', 'datafiles/images/class55.png', 'datafiles/images/class56.png', 'datafiles/images/class57.png', 'datafiles/images/class58.png', 'datafiles/images/class59.png', 
+           'datafiles/images/class60.png', 'datafiles/images/class61.png', 'datafiles/images/class62.png', 'datafiles/images/class63.png', 'datafiles/images/class64.png', 'datafiles/images/class65.png', 
+           'datafiles/images/class66.png', 'datafiles/images/class67.png', 'datafiles/images/class68.png', 'datafiles/images/class69.png', 'datafiles/images/class70.png', 'datafiles/images/class71.png', 
+           'datafiles/images/class72.png', 'datafiles/images/class73.png', 'datafiles/images/class74.png', 'datafiles/images/class75.png']
+
+kart_classes = ['datafiles/images/classk1.png', 'datafiles/images/classk2.png', 'datafiles/images/classk3.png', 'datafiles/images/classk4.png']
+
+rx_classes = ['datafiles/images/classrx1.png', 'datafiles/images/classrx2.png', 'datafiles/images/classrx3.png']
 
 tracks = ['Adelaide', 'Adelaide Historic 1988', 'Azure Circuit', 'Barcelona GP (no chicane)', 'Barcelona GP', 'Barcelona GP National', 'Bathurst 2020', 'Brands Hatch', 'Brands Hatch Indy', 'Brasilia Full',
           'Brasilia Outer', 'Buenos Aires no.6 S', 'Buenos Aires no.6', 'Buenos Aires no.7', 'Buenos Aires no.8', 'Buenos Aires no.9', 'Buenos Aires no.12', 'Buenos Aires no.15', 'Cadwell Park', 'Campo Grande',
@@ -33,7 +41,9 @@ kart_tracks = ['Buskerud Kart Long', 'Buskerud Kart Short', 'Granja Vianna Stage
                'Interlagos Kart Three', 'Londrina Kart One', 'Londrina Kart Two', 'Ortona Kart One', 'Ortona Kart Two', 'Ortona Kart Three', 'Ortona Kart Four', 'Speedland Kart 1', 'Speedland Kart 2',
                'Speedland Kart 3', 'Speedland Kart 4']
 
-rx_tracks = [
-        "Ascurra RX", "Tikki RX", "Foz", "Barcelona RX", "Hockenheim Rallycross", "Spa RX", "Nurburgring RX", "Ascurra Dirt", "Tikki Tarmac", "Foz", "Tikki Dirt 2", "Spa RX"]
+rx_tracks = ["Ascurra RX", "Tikki RX", "Foz", "Barcelona RX", "Hockenheim Rallycross", "Spa RX", "Nurburgring RX", "Ascurra Dirt", "Tikki Tarmac", "Foz", "Tikki Dirt 2", "Spa RX"]
 
-weather = []
+weather = ['datafiles/images/weather_null.png', 'datafiles/images/weather00.png', 'datafiles/images/weather01.png', 'datafiles/images/weather02.png', 'datafiles/images/weather03.png', 
+           'datafiles/images/weather04.png', 'datafiles/images/weather05.png', 'datafiles/images/weather06.png', 'datafiles/images/weather07.png', 'datafiles/images/weather08.png', 
+           'datafiles/images/weather09.png', 'datafiles/images/weather10.png', 'datafiles/images/weather11.png', 'datafiles/images/weather12.png', 'datafiles/images/weather13.png', 
+           'datafiles/images/weather14.png']
--- main.py
+++ main.py
@@ -30,6 +30,11 @@ class MainScreen(Screen):
         opponent_class = 'singleclass'
         race_length = 'short'
         weather = 'raining'
+        car_class1 = 'datafiles/images/class_null.png'
+        car_class2 = 'datafiles/images/class_null.png'
+        car_class3 = 'datafiles/images/class_null.png'
+        car_class4 = 'datafiles/images/class_null.png'
+        car_class5 = 'datafiles/images/class_null.png'
 
         if self.ids.singleclass.active == True: opponent_class = 'singleclass'
         elif self.ids.multiclass.active == True: opponent_class = 'multiclass'
@@ -42,8 +47,45 @@ class MainScreen(Screen):
         if self.ids.raining.active == True: weather = 'raining'
         elif self.ids.norain.active == True: weather = 'norain'
 
-def dlc_check():
-    pass
+        if opponent_class == 'singleclass':
+            class_type = random.choice(['normal', 'normal', 'normal', 'kart', 'normal', 'normal', 'normal', 'rx', 'normal', 'normal'])
+            car_class1 = self.class_check(class_type)
+            self.ids.class1.source = car_class1
+
+    def class_check(self, class_type):
+        json_file = open("datafiles/dlcs.json", 'r')
+        dlc_file = json.load(json_file)
+
+        random.shuffle(db.classes)
+        random.shuffle(db.kart_classes)
+        random.shuffle(db.rx_classes)
+
+        if dlc_file['Adrenaline pack pt1'] == False:
+            class_type = 'kart'
+
+
+        if class_type == 'normal': choosen_class = random.choice(db.classes)
+        elif class_type == 'kart': choosen_class = random.choice(db.kart_classes)
+        elif class_type == 'rx': choosen_class = random.choice(db.rx_classes)
+        
+
+        if dlc_file["Endurance pt1"] == False:
+            if 'class46' in choosen_class: return self.class_check()
+            elif 'class53' in choosen_class: return self.class_check()
+        
+        if dlc_file["Formula Hitech"] == False:
+            if 'class14' in choosen_class: return self.class_check()
+            elif 'class15' in choosen_class: return self.class_check()
+
+        if dlc_file["Racin'USA pt1"] == False:
+            if 'class07' in choosen_class: return self.class_check()
+            elif 'class49' in choosen_class: return self.class_check()
+
+        if dlc_file["Super Cars pt1"] == False:
+            if 'class13' in choosen_class: return self.class_check()
+
+        return choosen_class
+        
 
 class WindowManager(ScreenManager):
     pass
