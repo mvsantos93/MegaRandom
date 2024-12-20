787b910c04480e8bc10db68b86c72b8cbca62c9e
Marcos Santos <129782135+mvsantos93@users.noreply.github.com>
2024-12-20 18:04:51 -0300

    update

---
+++ Scripts/database.gd
@@ -0,0 +1,25 @@
+extends Node
+
+var class_null = "res://Images/Classes/class_null.jpg"
+
+var caterhams = ["4 classes"]
+var fclassic = ["7 classes"] #v12, hitech g1 e g2 entra aqui
+var fmodern = ["3 classes"]
+var fretro = ["3 classes"]
+var fv10 = ["2 classes"]
+var fvintage = ["2 classes"]
+var gthistorical = ["# classes"]
+var gtmodern = ["# classes"]
+var kart = ["4 classes"]
+var prototypes = ["# classes"]
+var rally = ["# classes"]
+var stockhistorical = ["# classes"]
+var stockmodern = ["# classes"]
+var street = ["# classes"]
+var touringhistoric = ["# classes"]
+var touringmodern = ["# classes"]
+
+var weather_null = "res://Images/Weathers/weather_null.jpg"
+var weather_random = "res://Images/Weathers/weather_random.png"
+var clean_weathers = "# weathers"
+var rain_weathers = "# weathers"
